275fbc697f4ac906a2b0f786367f5860943e8f2b
lingtalfi <lingtalfi@gmail.com>
2020-04-06 09:43:21 +0200

    update initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+Panda headers (js client)
+===========
+2020-04-06
+
+A js helper to implement the [panda headers](https://github.com/lingtalfi/TheBar/blob/master/discussions/panda-headers-protocol.md) protocol.
+
+
+
+Install
+=======
+
+```js
+npm install panda-headers
+```
+
+
+
+
+Usage
+===========
+
+Assuming the following response headers given by the server:
+
+```txt 
+panda__arrays: tags,names
+panda_author: boris
+panda_names: alice, maurice
+panda_tags: judo, karate, 1__panda_comma__2__panda_comma__3__panda_comma__ soleil 
+```
+
+
+Then we can use the helper like this:
+
+```js
+const panda = require("panda-headers");
+console.log(panda.getPandaHeaders(headers));
+
+/**
+- author: "boris"​
+- names: Array [ "alice", "maurice" ]
+- tags: Array(3) [ "judo", "karate", "1,2,3, soleil" ]
+*/
+
+
+```
+
+
+
+
+History Log
+=============
+    
+- 1.0.0 -- 2020-04-06
+
+    - initial commit 
---
+++ index.js
@@ -0,0 +1,48 @@
+var pandaHeaders = {
+
+    getPandaHeaders: function (headers) {
+        var ret = {};
+        var arrays = [];
+
+        var escapeCommaAndTrim = function (str) {
+            return str.replace(/__panda_comma__/g, ",", str).trim();
+        };
+
+
+        if (headers.has("panda__arrays")) {
+            arrays = headers.get('panda__arrays').split(",");
+        }
+
+
+        for (let v of headers) {
+            var [key, value] = v;
+            if ("panda__arrays" === key) {
+                continue;
+            }
+
+            if (jsx.startsWith(key, "panda_")) {
+                key = key.substr(6);
+
+
+                if (jsx.inArray(key, arrays)) {
+                    if (false === jsx.arrayKeyExists(key, ret)) {
+                        ret[key] = [];
+                    }
+                    let values = value.split(",");
+                    for (let j in values) {
+                        let val = escapeCommaAndTrim(values[j]);
+                        ret[key].push(val);
+                    }
+                } else {
+                    value = escapeCommaAndTrim(value);
+                    ret[key] = value;
+                }
+            }
+
+        }
+        return ret;
+    },
+};
+
+
+modules.exports = pandaHeaders;
